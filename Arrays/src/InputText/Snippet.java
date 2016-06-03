54922c4d669d53d15e8e4eb0d33d62f9fd0a6ebb
lockbjson <caotruongtuan@gmail.com>
2016-06-03 09:10:28 +0700

    New update.

--- Arrays/src/InputText/Snippet.java
+++ Arrays/src/InputText/Snippet.java
@@ -10,14 +10,13 @@ public class Snippet {
 		String[] symbols = { "$1", "$2", "$3", "$4" };
 		String[] input2 = new String[3];
 		System.out.print("Input1: ");
-		for (int i = 0; i < input1.length; i++) {
-			input1[i] = keyboard.next();
-		}
+		for (int i = 0; i < input1.length; i++) { input1[i] = keyboard.next();}
 		System.out.print("Input2: ");
-		for (int i = 0; i < input2.length; i++) {
-			input2[i] = keyboard.next();
-			System.out.print(input2[i].replace(symbols[i], input1[i]));
-			System.out.print(" ");
+		for (int i = 0; i < input2.length; i++) { input2[i] = keyboard.next();
+			for (int j = 0; j < symbols.length; j++) {
+				if (input2[i].contains(symbols[j])) { input2[i] = input2[i].replace(symbols[j], input1[j]);}
+			}
+			System.out.print(input2[i] + " ");
 		}
 	}
 }
